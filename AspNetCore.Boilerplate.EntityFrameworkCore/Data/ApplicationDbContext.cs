3e5b77b723917239db1472ce9e27535b1f7981a5
samxxxxx <sam.x5@hotmail.com>
2024-12-29 18:06:19 +0800

    添加项目文件。

---
+++ AspNetCore.Boilerplate.EntityFrameworkCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AspNetCore.Boilerplate.Service.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Boilerplate.EntityFrameworkCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AspNetCoreBoilerplateUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
